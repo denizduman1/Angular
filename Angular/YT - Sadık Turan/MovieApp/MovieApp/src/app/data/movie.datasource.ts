df5cda423c5d8c78473d7aaa05401925a94bd2d2
denizduman1 <ce.denizduman2@gmail.com>
2023-01-15 22:39:45 +0300

    Component kısım -2

---
+++ Angular/YT - Sadık Turan/MovieApp/MovieApp/src/app/data/movie.datasource.ts
@@ -0,0 +1,7 @@
+import { Movie } from "./movie";
+
+export const Movies: Movie[] = [
+    {id: 1, name: "Esaretin Bedeli"},
+    {id: 2, name: "Kahramanlar"},
+    {id: 3, name: "Adanalı"}
+]
--- Angular/YT - Sadık Turan/MovieApp/MovieApp/src/app/movies/movies.component.ts
+++ Angular/YT - Sadık Turan/MovieApp/MovieApp/src/app/movies/movies.component.ts
@@ -1,9 +1,21 @@
 import { Component } from "@angular/core";
+import { Movie } from "../data/movie";
+import { Movies } from "../data/movie.datasource";
 
 @Component({
    selector: 'movies', // <movies></movies>
-   templateUrl: './movies.component.html'
+   templateUrl: './movies.component.html',
+   styles: [`
+      h2{
+         color: blue;
+      }
+   `]
 })
 export class MoviesComponent {
+   title = 'Movie List';
+   movies = Movies;
+   getTitle() {
+      return this.title;
+   }
 
 }
